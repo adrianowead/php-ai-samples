aa62d8379860a779834681aa79d61d3c64137d10
Adriano Maciel <adriano_mail@hotmail.com>
2021-09-06 19:06:03 -0300

    Criada classe do problema do caixeiro viajante

---
+++ src/app/Genetic/TravellingSalesman.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Genetic;
+
+use App\Genetic\Base\Genetic;
+use App\Genetic\Base\Individual;
+use Swoole\Coroutine\WaitGroup;
+
+final class TravellingSalesman extends Genetic{
+    private $requiredChromosomes = [
+        'left' => 0.0,
+        'right' => 0.0,
+    ];
+
+    private $mutateRules = [
+        'probability' => 0.1, // 0.1 > 1.0
+        // cromosomos que podem sofrer mutação e seus limites
+        'chromosomes' => [
+            'left' => [
+                'min' => 0.1,
+                'max' => 1.0,
+            ],
+            'right' => [
+                'min' => 0.1,
+                'max' => 1.0,
+            ],
+        ],
+    ];
+
+    /**
+     * Completa a população com base nos indivíduos existentes, se não existirem, cria indivíduos novos
+     * Cruzando os gênis deles e executando mutações aleatórias
+     */
+    public function fillPopulation(): void
+    {
+        if($this->population->count() >= $this->limitPopulation) return ;
+
+        $limitToFill = $this->limitPopulation - $this->population->count();
+
+        $doEvolution = $this->population->count() > 0;
+
+        $this->fillEmptyPopulation(
+            limitToFill: $limitToFill,
+            waitExec: true,
+        );
+
+        if($doEvolution) {
+
+        }
+    }
+
+    /**
+     * Completa a população até o limite informado
+     */
+    private function fillEmptyPopulation(int $limitToFill, bool $waitExec = false): void
+    {
+        $list = array_chunk(array_fill(0, $limitToFill, null), 10);
+
+        $ctx = $this;
+
+        // preenchimento da população em lotes
+        // processmento paralelo via coroutine
+        \Co\run(function() use ($ctx, $list, $waitExec) {
+            $wg = false;
+
+            if($waitExec) $wg = new WaitGroup();
+
+            foreach($list as $chunk) {
+                go(function() use ($ctx, $chunk, $wg, $waitExec) {
+                    if($waitExec) $wg->add();
+
+                    $count = sizeof($chunk);
+
+                    for($y = 0; $y < $count; $y++) {
+                        $individual = new Individual;
+
+                        foreach( $ctx->requiredChromosomes as $chromosome => $value ){
+                            $individual->$chromosome = $value;
+                        }
+
+                        $ctx->mutate($individual);
+
+                        $ctx->born($individual);
+                    }
+
+                    if($waitExec) $wg->done();
+                });
+            }
+
+            if($waitExec) $wg->wait(1);
+        });
+    }
+
+    /**
+     * Executa uma mutação nos indivíduos
+     * Neste caso seguindo especificações do problema do caixeiro viajante
+     */
+    public function mutate(Individual &$individual): void
+    {
+        $this->doMutate($individual);
+    }
+
+    /**
+     * Executa uma mutação aleatória no atributo relacionao a este problema
+     */
+    private function doMutate(Individual &$individual): void
+    {
+        $probability = mt_rand(1, 10) <= ( $this->mutateRules['probability'] * 10 );
+
+        if($probability) {
+            $list = array_keys($this->mutateRules['chromosomes']);
+            shuffle($list);
+
+            $chromosome = $list[0];
+
+            $individual->$chromosome = $this->randFloat(
+                $this->mutateRules['chromosomes'][$chromosome]['min'],
+                $this->mutateRules['chromosomes'][$chromosome]['max'],
+            );
+
+            $individual->fitness = $this->randFloat(0.2,0.9);
+        }
+    }
+
+    private function randFloat(float $min, float $max): float
+    {
+        $range = $max - $min;
+        $num = $min + $range * (mt_rand() / mt_getrandmax());
+
+        return $num;
+    }
+
+    /**
+     * Executa o cruzamento "genético" (atributos) dos indivíduos informados
+     * Neste caso seguindo especificações do problema do caixeiro viajante
+     */
+    public function crossover(Individual $a, Individual $b): Individual
+    {
+        return new Individual;
+    }
+
+    /**
+     * Cálculo de aptidão do indivíduo
+     */
+    public function fitness(): float
+    {
+        return 0.0;
+    }
+}
